8a7362e09c778947acf315488772865c14bca104
Hudakasim <030722088@std.izu.edu.tr>
2024-06-14 08:13:44 +0300

    hazir

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 var path = require('path');
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
@@ -18,17 +20,35 @@ con.connect(function(err) {
     console.log('Connected!!!');
 });
 
+app.get("/", function(req, res){
+	res.redirect("/anasayfa");
+});
+
 app.get('/anasayfa', (req, res) => {
-let kosul = req.query.kosul || '';
-	let sql = "SELECT * from HudaKasimoglu.ilceler where ilce_adi = '"+ kosul +"';";
+	let kosul = req.query.kosul || '';
+	let sql = "SELECT * from HudaKasimoglu.ilceler";
+	if(kosul){
+		sql += " where ilce_adi LIKE '%"+ kosul +"%';"
+	}
 	con.query(sql, (err, result) => {
 		if (err) throw err;
-		console.log(result);
-
-		res.render('anasayfa', { data: result, kosul: kosul });
+		else{
+			console.log(result);
+			res.render("main", {bilgiler: result, kosul: kosul ,page: 'anasayfa'});
+		}
 	});
-})
+});
 
+app.get('/iletisim', (req, res) => {
+	let nesne = {
+		name: req.query.ad || '',
+		surname: req.query.soyad || '',
+		city: req.query.sehir || '',
+		subject: req.query.konu || ''
+	};
+	console.log(`Ad: ${nesne.name}, Soyad: ${nesne.surname}, Şehir: ${nesne.city}, Konu: ${nesne.subject}`);
+	res.render("main", {page: 'iletisim'});
+});
 
 let port = 7088;
 app.listen(port, (err)=>{
